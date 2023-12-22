393a24b276a8a0bc6fca20225a82196ed6f47923
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2023-12-22 19:21:04 +0100

    feat: db migration

---
+++ migrations/20231222181528_initial_database_setup.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS quotes
+(
+    id          UUID PRIMARY KEY,
+    book        VARCHAR     NOT NULL,
+    quote       TEXT        NOT NULL,
+    inserted_at TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ NOT NULL,
+    UNIQUE (book, quote)
+);
